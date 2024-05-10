3cb14c20819ca42214e754e75b2dc43d4396a309
Sagor Chandro <117623409+sagorChndro@users.noreply.github.com>
2024-05-10 14:33:50 +0600

    Add files via upload

---
+++ choose_your_won_adventure.py
@@ -0,0 +1,31 @@
+name = input("Type your name: ")
+print("Welcome", name, "to this adventure!")
+answer = input("You are on a dirt road it has come to an end and you can go left or right. Which way would you like "
+               "to go?").lower()
+if answer == "left":
+    answer = input("You come to river you can walk around it or swim across? Type walk to walk around and swim to "
+                   "swim across: ").lower()
+    if answer == "swim":
+        print("You swam across and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles, ran out water and you lost the game.")
+    else:
+        print("Not a valid option. You lose!")
+elif answer == "right":
+    answer = input("You come to the bridge its looks wobbly do you want to cross it or head back (cross/back)? ")
+    if answer == "back":
+        print("You go back and lose.")
+    elif answer == "cross":
+        answer = "You cross the bridge and meet a stranger. Do you talk to them (yes/no)? "
+        if answer == "yes":
+            print("You talk to the stranger and they give you gold. You win!")
+        elif answer == "No":
+            print("You ignore the stranger and they are offended and You lose!")
+        else:
+            print("Not a valid option. You lose!")
+    else:
+        print("Not a valid option. You lose!")
+else:
+    print("Not a valid option. You lose!")
+
+print("Thank you for trying", name)
